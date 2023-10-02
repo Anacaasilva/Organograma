e7919e6ddecfe7ce0e2cd4d7dab72258aa68a3b0
Carol <anacarolina.oliveira1020@gmail.com>
2023-10-02 20:57:08 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './componentes/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite <code>src/App.js</code> e salve para recarregar.
         </p>
         <a
           className="App-link"
